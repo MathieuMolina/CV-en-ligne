2bfa606bdf0828f4755eafc59d2aa53c2d8cf7f7
MathieuMolina <molina.mathieu@hotmail.com>
2022-03-01 16:54:29 +0100

    feat header up/ancres

---
+++ app.js
@@ -0,0 +1,13 @@
+function backToTop(){
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        });
+    });
+}
+backToTop();
+
+
