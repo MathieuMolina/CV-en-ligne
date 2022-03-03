bbe615f24fbd865ed7836a457aa4a6770d6b142b
MathieuMolina <molina.mathieu@hotmail.com>
2022-03-03 15:31:39 +0100

    fix : header end

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+                                            // ANIMATIONS TIMELINE 
+
+const allRonds = document.querySelectorAll('.rond');
+const allBoxes = document.querySelectorAll('.box');
+
+const controller = new ScrollMagic.Controller();
+
+
+
+allBoxes.forEach(box => {
+
+    for(i =0; i < allRonds.length; i++){
+        if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
+
+            let tween = gsap.from(box, {y: -50, opacity :0, duration: 0.5})
+
+            let scene = new ScrollMagic.Scene({
+                triggerElement: allRonds[i],
+                reverse: false
+            })
+            .setTween(tween)
+            //.addIndicators()
+            .addTo(controller)
+        }
+    }
+})
+
+                                                // FONCTION POUR ANCRE RETOUR EN HAUT
 function backToTop(){
     const btn = document.querySelector('.btn');
     btn.addEventListener('click', () => {
