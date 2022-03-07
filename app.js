b81fbc98f9f00a15af21fbc1bdd0e2e9d066a072
MathieuMolina <molina.mathieu@hotmail.com>
2022-03-07 09:43:50 +0100

    feat: timeline base

--- app.js
+++ app.js
@@ -3,26 +3,28 @@
 const allRonds = document.querySelectorAll('.rond');
 const allBoxes = document.querySelectorAll('.box');
 
-const controller = new ScrollMagic.Controller();
-
-
+const controller = new ScrollMagic.Controller()
 
 allBoxes.forEach(box => {
 
-    for(i =0; i < allRonds.length; i++){
+    for(i = 0; i < allRonds.length; i++){
+
         if(allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')){
 
-            let tween = gsap.from(box, {y: -50, opacity :0, duration: 0.5})
+            let tween = gsap.from(box, {y: -50, opacity: 0, duration: 0.5})
 
             let scene = new ScrollMagic.Scene({
                 triggerElement: allRonds[i],
-                reverse: false
+                reverse: true
             })
             .setTween(tween)
-            //.addIndicators()
+            // .addIndicators()
             .addTo(controller)
+
         }
+
     }
+
 })
 
                               // FONCTION POUR ANCRE RETOUR EN HAUT
